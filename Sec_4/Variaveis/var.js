cdb69f0138561a3a87739c6b6d0749faa1402f99
TrueSuperthay <braguinh@gmail.com>
2023-10-23 23:12:41 -0300

    Variaveis

---
+++ Sec_4/Variaveis/var.js
@@ -0,0 +1,21 @@
+//Aula 1 de variáveis
+let nome = "Daniel";
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+let laranjas = 5;
+console.log(laranjas * laranjas);
+let um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
+//Aula 2 de variáveis
+var nome2 = "Figueiredo"; //Declaração descontinuada
+const nome3 = "Braga"; //Valor constante, não altera
+console.log(nome2 + " " + nome3)
+//Aula 4 de variáveis - convenção
+//let 3nome = valor; >> Forma incorreta de declarar variável
+//let @nome = valor; >> Forma incorreta de declarar variável
+let nomeCompleto = "Daniel Braga";
+console.log(nomeCompleto);
+//Aula 5 de variáveis - palavras reservadas
+//let break = "parar"; >> Forma incorreta de declarar variável (break)
+let break1 = "parar";
+console.log(break1);
