4e4a3dbcba7bf58143bad8eee53946a69ace659a
TrueSuperthay <braguinh@gmail.com>
2023-10-28 00:45:47 -0300

    Update imc.js

--- Ex. livre/Calc IMC/imc.js
+++ Ex. livre/Calc IMC/imc.js
@@ -6,13 +6,13 @@ if(peso == "" || altura == ""){
     alert("Digite as informações necessárias!");
 } else if(imc >= 40){
     alert(`Seu imc está em ${imc}, portanto você está com Obesidade classe III!`);
-} else if(imc >= 18.5 || imc <= 24.9){
+} else if(imc >= 18.5 && imc <= 24.9){
     alert(`Seu imc está em ${imc}, portanto você está no Peso Normal!`);
-} else if(imc >= 25 || imc <= 29.9){
+} else if(imc >= 25 && imc <= 29.9){
     alert(`Seu imc está em ${imc}, portanto você está com Sobrepeso!`);
-} else if(imc >= 30 || imc <= 34.9){
+} else if(imc >= 30 && imc <= 34.9){
     alert(`Seu imc está em ${imc}, portanto você está com Obesidade de Grau I!`);
-} else if(imc >= 35 || imc <= 39.9){
+} else if(imc >= 35 && imc <= 39.9){
     alert(`Seu imc está em ${imc}, portanto você está com Obesidade de Grau II!`);
 } else {
     alert("Não foi possível verificar a os valores fornecidos!");
