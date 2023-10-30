8366c5b47e2a072f551de54937a70074586d9412
TrueSuperthay <braguinh@gmail.com>
2023-10-30 15:45:34 -0300

    switch

---
+++ Sec_4/Estruturas de programacao/Estruturas de repeticao/switch/switch.js
@@ -0,0 +1,14 @@
+let nome = prompt("Digite seu nome");
+switch(nome){
+    case "Joao":
+        alert("Olá, João!");
+        break;
+    case "Daniel":
+        alert("Olá, Daniel!");
+        break;
+    case "Matheus":
+        alert("Olá, Matheus!");
+        break;
+    default:
+        alert("Nome não encontrado!");
+}
