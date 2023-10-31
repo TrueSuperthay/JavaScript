0a9195aa0d9b18a2f511f49e6accdfcac9407307
TrueSuperthay <braguinh@gmail.com>
2023-10-31 00:42:15 -0300

    Update ex10.js

--- Sec_5/ex10.js
+++ Sec_5/ex10.js
@@ -5,3 +5,18 @@ if (numero % 4 != 0){
 } else {
     alert(`O número ${numero} não é primo!`);
 }
+
+/*let numero = prompt("Digite um número");
+let contRestos = 0;
+
+for (let i = 1; i <= numero; i++){
+    if(numero % i == 0) {
+        contRestos++;
+        }
+}
+
+if (contRestos == 2){
+    alert(`O número ${numero} é primo`);
+}else{
+    alert(`O número ${numero} não é primo`);
+}*/
