98d86189de9aad3e5efd48105c82a9d4835434a1
TrueSuperthay <braguinh@gmail.com>
2023-10-30 23:48:09 -0300

    Update ex10

---
+++ Sec_5/ex09.js
@@ -0,0 +1,17 @@
+/*for(numero = 0; numero <= 50; ++numero){
+    if(numero % 2 == 0){
+        console.log(`O número ${numero} é PAR.`);
+    } else {
+        console.log(`O número ${numero} é IMPAR.`);
+    }
+}*/
+
+let numero = 0;
+while (numero <= 50){
+    if(numero % 2 == 0){
+        console.log(`O número ${numero} é PAR.`);
+    } else {
+        console.log(`O número ${numero} é IMPAR.`);
+    }
+    numero++
+}
---
+++ Sec_5/ex10.js
@@ -0,0 +1,7 @@
+let numero = prompt("Digite um número");
+
+if (numero % 4 != 0){
+    alert(`O número ${numero} é primo!`);
+} else {
+    alert(`O número ${numero} não é primo!`);
+}
