91427fc0b89eb603c0b17ccedd7a9884be22a8b2
TrueSuperthay <braguinh@gmail.com>
2023-11-08 14:35:09 -0300

    New: Funcoes

---
+++ Sec_6/definir funcoes/funcoes.js
@@ -0,0 +1,17 @@
+function imprimirNoConsole(){
+    //imprime no console o que esta dentro da funcao
+    console.log("Olá, Mundo!");
+}
+imprimirNoConsole();
+
+function imprimirNoConsole2(num){
+    console.log("O número informado é " + num);
+}
+imprimirNoConsole2(2);
+
+num = prompt("Informe um valor");
+function imprimePrompt(num){
+    console.log(num);
+}
+imprimePrompt(num);
+
