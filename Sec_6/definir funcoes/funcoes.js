9dce8da3d0f5e1b158a683a15150fe07b1645bec
TrueSuperthay <braguinh@gmail.com>
2023-11-10 17:55:16 -0300

    umpdate: functions

--- Sec_6/definir funcoes/funcoes.js
+++ Sec_6/definir funcoes/funcoes.js
@@ -9,9 +9,26 @@ function imprimirNoConsole2(num){
 }
 imprimirNoConsole2(2);
 
-num = prompt("Informe um valor");
-function imprimePrompt(num){
-    console.log(num);
-}
-imprimePrompt(num);
+// num = prompt("Informe um valor");
+// function imprimePrompt(num){
+//     console.log(num);
+// }
+// imprimePrompt(num);
 
+// escopo aninhado
+
+let a = 30;
+console.log(a);
+function multiplicar (x, y) {
+    let a = x * y;
+    console.log(a);
+    if (a > 10) {
+        let a = 0;
+        a++;
+        console.log(a);
+    } else {
+        let b = 5;
+        console.log(b);
+    }
+}
+multiplicar (0, 10);
