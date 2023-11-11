2311c0896537cc1ba87f69798fcb84b922b1b179
TrueSuperthay <braguinh@gmail.com>
2023-11-10 21:47:02 -0300

    Update argsOptions.js

--- Sec_6/argumentos opcionais/argsOptions.js
+++ Sec_6/argumentos opcionais/argsOptions.js
@@ -1,5 +1,5 @@
 function nomeComIdade (nome, idade) {
-    if (idade === undefined) {
+    if (idade === undefined || idade === "") {
         console.log (`O nome informado é ${nome}`);
     } else {
         console.log(`O nome é ${nome} e tem ${idade} anos.`); 
@@ -7,5 +7,5 @@ function nomeComIdade (nome, idade) {
 }
 let p_nome = prompt ("Digite o nome");
 let p_idade = prompt("Agora digite a idade");
-nomeComIdade (p_nome);
+nomeComIdade (p_nome); //Argumento "idade" não foi utilizado
 nomeComIdade (p_nome, p_idade);
