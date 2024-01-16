c1f96d7e1094ca8cfa124d06de02284aa96fa0ff
TrueSuperthay <braguinh@gmail.com>
2024-01-16 09:57:50 -0300

    Sec_8 NEW

---
+++ Sec_8/array.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let nomes = ['Matheus', 'João', 'Alexia'];
+let bool = [true, false, true];
+let misturado = [1, 'Matheus', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
