f566adf12b5e796f2ca602f0e4af5c752a3cb9bd
Danie Braga <64237798+TrueSuperthay@users.noreply.github.com>
2024-01-30 17:32:14 -0300

    Update obj.js

--- Sec_8/Objetos basic/obj.js
+++ Sec_8/Objetos basic/obj.js
@@ -2,14 +2,14 @@ let daniel = {
     nomeCompleto: 'Daniel Figueiredo Braga Fernandes',
     idade: 43,
     filhos: 2,
-    cor: 'Parda'
+    cor: 'Parda',
     patrimonio: function () {
         console.log("Carro");
     }
 }
 console.log(daniel.nomeCompleto);
 console.log(daniel.cor);
-daniel.patrimonio;
+daniel.patrimonio();
 
 // [] = array
 // {} = obj
