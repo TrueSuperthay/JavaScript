b2b10ee1e2d773167d142fbfd85bd6c9f0f57fa0
Danie Braga <64237798+TrueSuperthay@users.noreply.github.com>
2024-01-30 17:54:50 -0300

    UPDATE Sec 8

---
+++ Sec_8/Adicionando e removendo propriedades/adceremov.js
@@ -0,0 +1,12 @@
+let carro = {
+    marca: 'Ford',
+    modelo: 'KA',
+    ano: '2018/2019',
+    cor: 'Branco',
+    motor: 1.5
+}
+console.log(carro);
+
+delete carro.motor;
+
+carro.portas = 4;
---
+++ Sec_8/Copiando propriedades/cpyprpobj.js
@@ -0,0 +1,15 @@
+let carro = {
+    marca: 'Ford',
+    modelo: 'KA',
+    ano: 2019,
+    Portas: 4
+}
+let adicionais = {
+    arCondicionado: true,
+    travaEletrica: true,
+    vidroEletrico: true
+}
+
+console.log(carro);
+
+//Object.assign(carro, adicionais);
